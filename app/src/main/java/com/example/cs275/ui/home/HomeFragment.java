4336797637e62586b7c83742f393e84d8cdae1cb
VanessaWhite26 <Khfftwewy26>
2019-11-19 11:14:14 -0500

    Trying to make a permissions dialog

--- app/src/main/java/com/example/cs275/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cs275/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.cs275.ui.home;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -32,7 +34,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
-//    private HomeViewModel homeViewModel;
+    //    private HomeViewModel homeViewModel;
     private static final int MY_LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
@@ -46,9 +48,12 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
     private boolean userInTravel = false;
 
     private Button mButton;
+    private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        userPermission();
 //        homeViewModel =
 //                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -85,11 +90,11 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
                             String str = String.format("%.02f", distance);
                             tv.append(str + " meters away from Library");
                             // determine whether user in travel
-                            if (distance >= 500 && !userInTravel){
+                            if (distance >= 500 && !userInTravel) {
                                 tv.append("\nUser in travel");
                                 userInTravel = true;
                             }
-                            if (userInTravel && distance <= 100){
+                            if (userInTravel && distance <= 100) {
                                 tv.append("\nYou are back home");
                                 userInTravel = false;
                             }
@@ -105,10 +110,10 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
     }
 
     @Override
-    public void onClick(View view)
-    {
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_send:
+                userPermission();
                 // Do something
 //                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 //                builder.setTitle("Title");
@@ -133,14 +138,94 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
         }
     }
 
+    private void userPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle("Required Location Permission")
+                            .setMessage("You have to give this app permission to access feature")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+
+                                }
+                            })
+                            .create()
+                            .show();
+
+
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+
+        }
+
+    } else {
+        // Permission has already been granted
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            lat = location.getLatitude();
+                            lon = location.getLongitude();
+                            curr.setLatitude(lat);
+                            curr.setLongitude(lon);
+                            distance = library.distanceTo(curr);
+                            String str = String.format("%.02f", distance);
+                            tv.append(str + " meters away from Library");
+                            // determine whether user in travel
+                            if (distance >= 500 && !userInTravel) {
+                                tv.append("\nUser in travel");
+                                userInTravel = true;
+                            }
+                            if (userInTravel && distance <= 100) {
+                                tv.append("\nYou are back home");
+                                userInTravel = false;
+                            }
+//                            tv.append("\nCurrent local: " + String.format(Locale.US, "%s : %s", lat, lon));
+                        }
+                    }
+                });
+    }
+    }
+
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
         LatLng vermont = new LatLng(44, -73);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(vermont));
         googleMap.addMarker(new MarkerOptions().position(libraryLoc).title("Library"));
 
         // current location
+        LatLng current = new LatLng(curr.getLatitude(),curr.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(current);
+        markerOptions.title("My Location");
+
+        googleMap.addMarker(markerOptions);
         mMap.setMyLocationEnabled(true);
     }
 
@@ -156,5 +241,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
             }
         }
     }
+
+
 
 }
