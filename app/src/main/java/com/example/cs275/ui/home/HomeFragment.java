975ef685aa124e30122b231d4214831b34f36e4d
maxwellpeck <maxwell.peck@uvm.edu>
2019-12-03 03:07:27 -0500

    Added input validation, error handling, and "helper" status messages to help user find better search terms if desired location is not found

--- app/src/main/java/com/example/cs275/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cs275/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.cs275.ui.home;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
 import android.os.Bundle;
 import android.text.InputType;
@@ -18,6 +19,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.cs275.DatabaseHelper;
 import com.example.cs275.MainActivity;
 import com.example.cs275.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -30,22 +32,28 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+//==================================================================================================
+
 public class HomeFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
 
 //    private HomeViewModel homeViewModel;
     private static final int MY_LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
-    private TextView tv;
+//    private TextView tv;
     private double lat = 0.0, lon = 0.0;
-    private LatLng libraryLoc = new LatLng(44.477239, -73.196687);
-    private Location library = new Location("Library");
+    private LatLng homeLatLng;
+    private Location homeLoc = new Location("Home");
+//    private LatLng libraryLoc = new LatLng(44.477239, -73.196687);
+//    private Location library = new Location("Library");
     private Location curr = new Location("curr");
     // distance to store distance between home and current location in meters
-    private double distance = 0.0;
+//    private double distance = 0.0;
     private boolean userInTravel = false;
 
-    private Button mButton;
+//    private Button mButton;
+
+    //==============================================================================================
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -59,14 +67,16 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
 //                textView.setText(s);
 //            }
 //        });
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //
-        tv = root.findViewById(R.id.text);
-        library.setLatitude(44.477239);
-        library.setLongitude(-73.196687);
+
+
+//        tv = root.findViewById(R.id.text);
+//        library.setLatitude(44.477239);
+//        library.setLongitude(-73.196687);
 
         // get user current location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
@@ -81,68 +91,75 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
                             lon = location.getLongitude();
                             curr.setLatitude(lat);
                             curr.setLongitude(lon);
-                            distance = library.distanceTo(curr);
-                            String str = String.format("%.02f", distance);
-                            tv.append(str + " meters away from Library");
-                            // determine whether user in travel
-                            if (distance >= 500 && !userInTravel){
-                                tv.append("\nUser in travel");
-                                userInTravel = true;
-                            }
-                            if (userInTravel && distance <= 100){
-                                tv.append("\nYou are back home");
-                                userInTravel = false;
-                            }
+//                            distance = library.distanceTo(curr);
+//                            String str = String.format("%.02f", distance);
+//                            tv.append(str + " meters away from Library");
+//                            // determine whether user in travel
+//                            if (distance >= 500 && !userInTravel){
+//                                tv.append("\nUser in travel");
+//                                userInTravel = true;
+//                            }
+//                            if (userInTravel && distance <= 100){
+//                                tv.append("\nYou are back home");
+//                                userInTravel = false;
+//                            }
 //                            tv.append("\nCurrent local: " + String.format(Locale.US, "%s : %s", lat, lon));
                         }
                     }
                 });
 
         // Button
-        mButton = root.findViewById(R.id.button_send);
+//        mButton = root.findViewById(R.id.button_send);
 
         return root;
     }
 
+    //==============================================================================================
+
     @Override
     public void onClick(View view)
     {
-        switch (view.getId()) {
-            case R.id.button_send:
-                // Do something
-//                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//                builder.setTitle("Title");
-//                final EditText input = new EditText(getActivity());
-//                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-//                builder.setView(input);
-//                // Set up the buttons
-//                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        m_Text = input.getText().toString();
-//                    }
-//                });
-//                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.cancel();
-//                    }
-//                });
-//
-//                builder.show();
-        }
+//        switch (view.getId()) {
+//            case R.id.button_send:
+//                // Do something
+////                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+////                builder.setTitle("Title");
+////                final EditText input = new EditText(getActivity());
+////                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+////                builder.setView(input);
+////                // Set up the buttons
+////                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+////                    @Override
+////                    public void onClick(DialogInterface dialog, int which) {
+////                        m_Text = input.getText().toString();
+////                    }
+////                });
+////                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+////                    @Override
+////                    public void onClick(DialogInterface dialog, int which) {
+////                        dialog.cancel();
+////                    }
+////                });
+////
+////                builder.show();
+//        }
     }
+
+    //==============================================================================================
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng vermont = new LatLng(44, -73);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(vermont));
-        googleMap.addMarker(new MarkerOptions().position(libraryLoc).title("Library"));
+//        LatLng vermont = new LatLng(44, -73);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(homeLatLng));
+//        googleMap.addMarker(new MarkerOptions().position(libraryLoc).title("Library"));
+//        googleMap.addMarker(new MarkerOptions().position(homeLatLng).title("Home"));
 
         // current location
         mMap.setMyLocationEnabled(true);
     }
+
+    //==============================================================================================
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -156,5 +173,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
             }
         }
     }
+
+    //==============================================================================================
 
 }
--- app/src/main/java/com/example/cs275/ui/launch/LaunchFragment.java
+++ app/src/main/java/com/example/cs275/ui/launch/LaunchFragment.java
@@ -20,7 +20,6 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cs275.DatabaseHelper;
-import com.example.cs275.MainActivity;
 import com.example.cs275.R;
 
 import java.io.IOException;
@@ -48,6 +47,10 @@ public class LaunchFragment extends Fragment {
         final EditText email = (EditText) root.findViewById(R.id.text_email);
         final EditText search = (EditText) root.findViewById(R.id.text_geocodeSearch);
         final TextView statusTextView = (TextView) root.findViewById(R.id.text_status);
+        final TextView searchResultsTextView = (TextView) root.findViewById(R.id.text_result);
+        final TextView searchResultsStatusTextView = (TextView) root.findViewById(R.id.text_result_status);
+
+        statusTextView.setTextColor(Color.BLACK);
 
         final String status = "Please input the below information:";
 //        String nameMsg = "Name";
@@ -70,7 +73,7 @@ public class LaunchFragment extends Fragment {
 //            }
 //        });
 
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
         //==========================================================================================
 
@@ -86,20 +89,36 @@ public class LaunchFragment extends Fragment {
                 String userNameString = userName.getText().toString();
                 String emailString = email.getText().toString();
 
+                String searchText = search.getText().toString();
+                List<String> list;
+                try {
+                    list = geolocate(searchText);
+                    String testForError = list.get(0);
+                    if (testForError.equals("anything")) {
+                        //May cause error
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    list = new ArrayList<>();
+                    for (int i = 0; i < 4; i++) {
+                        list.add(i, "error");
+                    }
+                }
+
                 //----------------------------------------------------------------------------------
 
-                //If email is not in proper format:
-                if (!emailString.contains("@") || !emailString.contains(".") || userNameString.length() == 0) {
+                //If input is not in proper format:
+                if (!emailString.contains("@") || !emailString.contains(".") || userNameString.length() == 0 || list.get(0).equals("error") || list.size() < 5) {
                     String status = "Check the format of your input!";
                     statusTextView.setText(status);
                     statusTextView.setTextColor(Color.RED);
 
                 } else { //-------------------------------------------------------------------------
-                    //If email is in proper format:
+                    //If input is in proper format:
 
                     //Add user input to database:
                     DatabaseHelper db = new DatabaseHelper(getActivity());
-                    db.insertData(userNameString, emailString, "surveyTime", "homeLocality", "homeLatitude", "homeLongitude");
+                    db.insertData(userNameString, emailString, "surveyTime", list.get(0), list.get(3), list.get(4));
 //                    Cursor cur = db.getAllData();
 //                if (cur.moveToFirst()) {
 //                    System.out.println(cur.getString(cur.getColumnIndex("NAME")));
@@ -121,16 +140,32 @@ public class LaunchFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                String searchResultsTextViewString = "No Results, please try searching again!";
+                String searchResultsStatusTextViewString = "If you are searching for a City name, try adding the state/province to be more specific!";
                 //Retrieve strings inputted by user in EditText field:
                 String searchText = search.getText().toString();
                 try {
                     List<String> list = geolocate(searchText);
-                    System.out.println(list.get(0));
-                    System.out.println(list.get(1));
-                    System.out.println(list.get(2));
-                } catch (IOException e) {
+
+                    //If no search results were found:
+                    if (list.get(0).equals("No results!")) {
+                        //Do something
+                    } else if (list.get(0).equals("Be more specific!")) { //If multiple results were found:
+                        //Do something
+                    } else { //If search results were found:
+                        searchResultsTextViewString = "City Found: " + list.get(0) + ", " + list.get(1) + ", " + list.get(2);
+                        searchResultsStatusTextViewString = "If this location is correct, please continue by clicking Submit!\nIf the location is incorrect, try searching again!";
+//                    System.out.println(list.get(0));
+//                    System.out.println(list.get(1));
+//                    System.out.println(list.get(2));
+                    }
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+                searchResultsTextView.setText(searchResultsTextViewString);
+                searchResultsStatusTextView.setText(searchResultsStatusTextViewString);
+                searchResultsTextView.setTextColor(Color.BLACK);
+                searchResultsStatusTextView.setTextColor(Color.BLACK);
             }
         });
 
@@ -150,15 +185,30 @@ public class LaunchFragment extends Fragment {
 
     public List<String> geolocate(String searchTerm) throws IOException {
         Geocoder gc = new Geocoder(getContext());
-        List<Address> addressList = gc.getFromLocationName(searchTerm, 1);
-        Address address = addressList.get(0);
-        String locality = address.getLocality();
-        double lat = address.getLatitude();
-        double lon = address.getLongitude();
         List<String> list = new ArrayList<>();
-        list.add(String.valueOf(lat));
-        list.add(String.valueOf(lon));
-        list.add(locality);
+        List<Address> addressList = gc.getFromLocationName(searchTerm, 1);
+
+        //If search results were found:
+        if (addressList.size() > 0) {
+            try {
+                Address address = addressList.get(0);
+                String locality = address.getLocality();
+                String state = address.getAdminArea();
+                String country = address.getCountryName();
+                double lat = address.getLatitude();
+                double lon = address.getLongitude();
+                list.add(locality);
+                list.add(state);
+                list.add(country);
+                list.add(String.valueOf(lat));
+                list.add(String.valueOf(lon));
+            } catch (NullPointerException e) {
+                list.add(0, "Be more specific!");
+            }
+
+        } else { //If no search results were found:
+            list.add("No results!");
+        }
         return list;
     }
 
